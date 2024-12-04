33be871d6b71824f9937593b8a5cb1258b2aefb0
Melanie May <melaniemay@loopreturns.com>
2024-12-04 14:31:37 -0500

    remove extra output from debugging

--- 4/pt2.php
+++ 4/pt2.php
@@ -15,7 +15,7 @@ for ($i = 0; $i < strlen($input); $i += $lineLength + 1) {
 }
 
 $total = find($lines);
-echo $total; // 2536
+echo $total; // 1875
 
 /**
  * @param string[] $lines
@@ -43,22 +43,17 @@ function find(array $lines): int
 function check(array $lines, int $row, int $col): bool
 {
     $pos1a = $lines[$row][$col];
-    $pos2a = $lines[$row][$col + 2];
-
-    $mid = $lines[$row + 1][$col + 1]; // must be a
-
     $pos1b = $lines[$row + 2][$col + 2];
+
+    $pos2a = $lines[$row][$col + 2];
     $pos2b = $lines[$row + 2][$col];
 
-    if ($mid !== 'A') return false;
+    if ($lines[$row + 1][$col + 1] !== 'A') return false;
 
     if (
         (($pos1a == 'M' && $pos1b == 'S') || ($pos1a == 'S' && $pos1b == 'M')) &&
         (($pos2a == 'M' && $pos2b == 'S') || ($pos2a == 'S' && $pos2b == 'M'))
     ) {
-        echo $lines[$row][$col] . "-" . $lines[$row][$col + 2] . "\n";
-        echo "-" . $lines[$row + 1][$col + 1] . "-" . "\n";
-        echo $lines[$row + 2][$col] . "-" . $lines[$row + 2][$col + 2] . "\n";
         return true;
     }
 
