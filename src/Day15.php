f618dd41fdcfe1a6a70d773a53d0c54478122aa7
Melanie May <melanie@MacBookPro.lan>
2024-12-17 15:20:07 -0500

    Day 15 Pt 1

--- src/Day15.php
+++ src/Day15.php
@@ -6,7 +6,19 @@ use RuntimeException;
 
 class Day15
 {
-    private string $input;
+    /**
+     * @var array<int, array<int, string>>
+     */
+    private array $grid;
+
+    /**
+     * @var array<int, string>
+     */
+    private array $moves;
+
+    private int $robotX;
+
+    private int $robotY;
 
     /**
      * @param  string  $file
@@ -21,15 +33,136 @@ class Day15
 
         if ($input === false) {
             exit('Failed to read input file');
-        } else {
-            $this->input = $input;
         }
 
-        $this->processInput();
+        $this->processInput($input);
     }
 
-    private function processInput(): void
+    private function processInput(string $input): void
     {
-        $input = $this->input;
+        [$gridStr, $movesStr] = explode("\n\n", trim($input));
+
+        $lines = explode("\n", $gridStr);
+        foreach ($lines as $y => $line) {
+            $this->grid[$y] = str_split($line);
+            if (($x = strpos($line, '@')) !== false) {
+                $this->robotX = $x;
+                $this->robotY = $y;
+            }
+        }
+
+        $this->moves = str_split(str_replace("\n", '', $movesStr));
+    }
+
+    public function solvePart1(): int
+    {
+        foreach ($this->moves as $move) {
+            $this->processMove($move);
+        }
+
+        return $this->calculateGPSSum();
+    }
+
+    private function processMove(string $move): void
+    {
+        $dx = $dy = 0;
+
+        switch ($move) {
+            case '^': $dy = -1;
+                break;
+            case 'v': $dy = 1;
+                break;
+            case '<': $dx = -1;
+                break;
+            case '>': $dx = 1;
+                break;
+        }
+
+        $newX = $this->robotX + $dx;
+        $newY = $this->robotY + $dy;
+
+        if ($this->canMove($newX, $newY, $dx, $dy)) {
+            $this->moveRobot($newX, $newY);
+        }
+    }
+
+    private function canMove(int $x, int $y, int $dx, int $dy): bool
+    {
+        if (! isset($this->grid[$y][$x])) {
+            return false;
+        }
+
+        $targetCell = $this->grid[$y][$x];
+
+        if ($targetCell === '#') {
+            return false;
+        }
+
+        if ($targetCell === '.' || $targetCell === '@') {
+            return true;
+        }
+
+        if ($targetCell === 'O') {
+            $nextX = $x + $dx;
+            $nextY = $y + $dy;
+
+            if (! isset($this->grid[$nextY][$nextX])) {
+                return false;
+            }
+
+            $nextCell = $this->grid[$nextY][$nextX];
+            if ($nextCell === '.') {
+                return true;
+            }
+            if ($nextCell === 'O') {
+                return $this->canMove($nextX, $nextY, $dx, $dy);
+            }
+        }
+
+        return false;
+    }
+
+    private function moveRobot(int $newX, int $newY): void
+    {
+        $dx = $newX - $this->robotX;
+        $dy = $newY - $this->robotY;
+
+        if ($this->grid[$newY][$newX] === 'O') {
+            $boxPositions = [];
+            $checkX = $newX;
+            $checkY = $newY;
+
+            while (isset($this->grid[$checkY][$checkX]) && $this->grid[$checkY][$checkX] === 'O') {
+                $boxPositions[] = [$checkX, $checkY];
+                $checkX += $dx;
+                $checkY += $dy;
+            }
+
+            for ($i = count($boxPositions) - 1; $i >= 0; $i--) {
+                [$boxX, $boxY] = $boxPositions[$i];
+                $this->grid[$boxY + $dy][$boxX + $dx] = 'O';
+                $this->grid[$boxY][$boxX] = '.';
+            }
+        }
+
+        $this->grid[$newY][$newX] = '@';
+        $this->grid[$this->robotY][$this->robotX] = '.';
+
+        $this->robotX = $newX;
+        $this->robotY = $newY;
+    }
+
+    private function calculateGPSSum(): int
+    {
+        $sum = 0;
+        foreach ($this->grid as $y => $row) {
+            foreach ($row as $x => $cell) {
+                if ($cell === 'O') {
+                    $sum += (100 * $y + $x);
+                }
+            }
+        }
+
+        return $sum;
     }
 }
--- tests/Unit/Day15Test.php
+++ tests/Unit/Day15Test.php
@@ -20,13 +20,52 @@ class Day15Test extends TestCase
         $dayFifteen = new Day15($this->file);
 
         $reflection = new \ReflectionClass($dayFifteen);
-        $inputProperty = $reflection->getProperty('input');
-        $inputProperty->setAccessible(true);
-        $input = $inputProperty->getValue($dayFifteen);
+        $gridProperty = $reflection->getProperty('grid');
+        $gridProperty->setAccessible(true);
+        $grid = $gridProperty->getValue($dayFifteen);
 
-        $expected = '';
+        $expectedGrid = '##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########';
+        $expectedGrid = explode("\n", $expectedGrid);
+        $expectedGrid = array_map(fn ($line) => str_split($line), $expectedGrid);
 
-        $this->assertEquals($expected, $input);
+        $movesProperty = $reflection->getProperty('moves');
+        $movesProperty->setAccessible(true);
+        $moves = $movesProperty->getValue($dayFifteen);
+
+        $expectedMoves = '<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^';
+
+        $expectedMoves = str_split(str_replace("\n", '', $expectedMoves));
+
+        $robotXProperty = $reflection->getProperty('robotX');
+        $robotXProperty->setAccessible(true);
+        $robotX = $robotXProperty->getValue($dayFifteen);
+
+        $robotYProperty = $reflection->getProperty('robotY');
+        $robotYProperty->setAccessible(true);
+        $robotY = $robotYProperty->getValue($dayFifteen);
+
+        $this->assertEquals($expectedGrid, $grid);
+        $this->assertEquals($expectedMoves, $moves);
+        $this->assertEquals(4, $robotX);
+        $this->assertEquals(4, $robotY);
     }
 
     public function test_constructor_throws_exception_if_file_is_not_found(): void
@@ -35,5 +74,11 @@ class Day15Test extends TestCase
         $this->expectExceptionMessage('File not found');
 
         new Day15('nonexistent.txt');
+    }
+
+    public function test_solve_part1_returns_correct_result(): void
+    {
+        $dayFifteen = new Day15($this->file);
+        $this->assertEquals(10092, $dayFifteen->solvePart1());
     }
 }
