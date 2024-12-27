b3cf701ce7523af98f47586c23c60de941fec35d
Melanie May <melanie@Melanies-MacBook-Pro-2.local>
2024-12-27 18:11:29 -0500

    Day 25

--- src/Day25.php
+++ src/Day25.php
@@ -6,7 +6,15 @@ use RuntimeException;
 
 class Day25
 {
-    private string $input;
+    /**
+     * @var array<int<0, max>, array<int, int>>
+     */
+    private array $keys = [];
+
+    /**
+     * @var array<int<0, max>, array<int, int>>
+     */
+    private array $locks = [];
 
     /**
      * @param  string  $file
@@ -21,15 +29,130 @@ class Day25
 
         if ($input === false) {
             exit('Failed to read input file');
-        } else {
-            $this->input = $input;
         }
 
-        $this->processInput();
+        $this->processInput($input);
     }
 
-    private function processInput(): void
+    private function processInput(string $input): void
     {
-        $input = $this->input;
+        $schematics = explode("\n\n", $input);
+        $this->processSchematics($schematics);
+    }
+
+    /**
+     * @param  array<array-key, string>  $schematics
+     */
+    private function processSchematics($schematics): void
+    {
+        $locks = [];
+        $keys = [];
+
+        foreach ($schematics as $schematic) {
+            $lines = explode("\n", $schematic);
+            if ($lines[0] == '#####') {
+                $locks[] = $lines;
+            } else {
+                $keys[] = $lines;
+            }
+        }
+
+        $this->processLocks($locks);
+        $this->processKeys($keys);
+    }
+
+    /**
+     * @param  array<int, array<int, string>>  $locks
+     */
+    private function processLocks($locks): void
+    {
+        $processedLocks = [];
+        $ret = [];
+        $i = 0;
+        foreach ($locks as $lock) {
+            foreach ($lock as $line) {
+                $processedLocks[$i][] = str_split($line);
+            }
+            $i++;
+        }
+
+        $h = count($processedLocks[0]);
+        $d = count($processedLocks[0][0]);
+
+        foreach ($processedLocks as $k => $lock) {
+            for ($i = 0; $i < $d; $i++) {
+                $x = 0;
+                for ($j = 0; $j < $h; $j++) {
+                    if ($lock[$j][$i] == '#') {
+                        $x++;
+                    }
+                }
+                $ret[$k][] = $x - 1;
+            }
+        }
+        $this->locks = $ret;
+    }
+
+    /**
+     * @param  array<int, array<int, string>>  $keys
+     */
+    private function processKeys($keys): void
+    {
+        $processedKeys = [];
+        $ret = [];
+        $i = 0;
+        foreach ($keys as $key) {
+            foreach ($key as $line) {
+                $processedKeys[$i][] = str_split($line);
+            }
+            $i++;
+        }
+
+        $h = count($processedKeys[0]);
+        $d = count($processedKeys[0][0]);
+
+        foreach ($processedKeys as $k => $key) {
+            for ($i = 0; $i < $d; $i++) {
+                $x = 0;
+                for ($j = 0; $j < $h; $j++) {
+                    if ($key[$j][$i] == '#') {
+                        $x++;
+                    }
+                }
+                $ret[$k][] = $x - 1;
+            }
+        }
+        $this->keys = $ret;
+    }
+
+    private function checkKey(int $key, int $lock): bool
+    {
+        return $key + $lock <= 5;
+    }
+
+    public function part1(): int
+    {
+        $total = 0;
+        $pass = true;
+
+        $pos = count($this->keys[0]);
+
+        foreach ($this->keys as $key) {
+            foreach ($this->locks as $lock) {
+                for ($i = 0; $i < $pos; $i++) {
+                    if ($this->checkKey($key[$i], $lock[$i])) {
+                        $pass = true;
+                    } else {
+                        $pass = false;
+                        break;
+                    }
+                }
+                if ($pass) {
+                    $total++;
+                }
+            }
+        }
+
+        return $total;
     }
 }
--- tests/Unit/Day25Test.php
+++ tests/Unit/Day25Test.php
@@ -20,13 +20,27 @@ class Day25Test extends TestCase
         $dayTwentyFive = new Day25($this->file);
 
         $reflection = new \ReflectionClass($dayTwentyFive);
-        $inputProperty = $reflection->getProperty('input');
-        $inputProperty->setAccessible(true);
-        $input = $inputProperty->getValue($dayTwentyFive);
+        $keysProperty = $reflection->getProperty('keys');
+        $keysProperty->setAccessible(true);
+        $keys = $keysProperty->getValue($dayTwentyFive);
 
-        $expected = '';
+        $locksProperty = $reflection->getProperty('locks');
+        $locksProperty->setAccessible(true);
+        $locks = $locksProperty->getValue($dayTwentyFive);
 
-        $this->assertEquals($expected, $input);
+        $expectedKeys = [
+            [5, 0, 2, 1, 3],
+            [4, 3, 4, 0, 2],
+            [3, 0, 2, 0, 1],
+        ];
+
+        $expectedLocks = [
+            [0, 5, 3, 4, 3],
+            [1, 2, 0, 5, 3],
+        ];
+
+        $this->assertEquals($expectedKeys, $keys);
+        $this->assertEquals($expectedLocks, $locks);
     }
 
     public function test_constructor_throws_exception_if_file_is_not_found(): void
@@ -35,5 +49,12 @@ class Day25Test extends TestCase
         $this->expectExceptionMessage('File not found');
 
         new Day25('nonexistent.txt');
+    }
+
+    public function test_part_one_returns_correct_value(): void
+    {
+        $dayTwentyFive = new Day25($this->file);
+
+        $this->assertEquals(3, $dayTwentyFive->part1());
     }
 }
